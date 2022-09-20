a0e477ce0b6a0546f4923cf7b5fc2240d6103a93
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-20 14:58:08 +0200

    task 1 add close on escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,15 +4,15 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
 const imagesMarkup = createImagesMarkup(galleryItems);
 
-galleryEl.insertAdjacentHTML('beforeend', imagesMarkup)
-
-galleryEl.addEventListener('click', onImageClick)
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryEl.addEventListener('click', onImageClick);
 
 function createImagesMarkup(galleryItems) {
     return galleryItems.map(image => `<div class="gallery__item"><a class="gallery__link" href="${image.original}">
      <img class="gallery__image" src="${image.preview}" data-source="${image.original}" 
      alt="${image.description}"/></a></div>`).join('');
 }
+
 
 function onImageClick(evt) {
     evt.preventDefault();
@@ -24,9 +24,12 @@ function onImageClick(evt) {
     <img src="${evt.target.dataset.source}">
 `)
     instance.show();
+    window.addEventListener('keydown', onKeyPress);
+
+    function onKeyPress(evt) {
+    if (instance.visible() && evt.code === "Escape") {
+        instance.close()
+        window.removeEventListener('keydown', onKeyPress)
+        }
+}
 } 
-
-
-
-
-console.log(galleryItems);
