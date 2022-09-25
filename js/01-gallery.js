b46db6af7157197f5e76f717404df76a2b5fa309
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-25 19:27:41 +0200

    add prittier for js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 const imagesMarkup = createImagesMarkup(galleryItems);
 
-galleryEl.insertAdjacentHTML('beforeend', imagesMarkup);
-galleryEl.addEventListener('click', onImageClick);
+galleryEl.insertAdjacentHTML("beforeend", imagesMarkup);
+galleryEl.addEventListener("click", onImageClick);
 
 function createImagesMarkup(galleryItems) {
-    return galleryItems.map(image => `<div class="gallery__item"><a class="gallery__link" href="${image.original}">
+  return galleryItems
+    .map(
+      (
+        image
+      ) => `<div class="gallery__item"><a class="gallery__link" href="${image.original}">
      <img class="gallery__image" src="${image.preview}" data-source="${image.original}" 
-     alt="${image.description}"/></a></div>`).join('');
+     alt="${image.description}"/></a></div>`
+    )
+    .join("");
 }
-
 
 function onImageClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    if (!evt.target.classList.contains('gallery__image')) {
-        return;
-    }
-    const instance = basicLightbox.create(`
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}">
-`)
-    instance.show();
-    window.addEventListener('keydown', onKeyPress);
+`);
+  instance.show();
+  window.addEventListener("keydown", onKeyPress);
 
-    function onKeyPress(evt) {
+  function onKeyPress(evt) {
     if (instance.visible() && evt.code === "Escape") {
-        instance.close()
-        
+      instance.close();
     }
-    window.removeEventListener('keydown', onKeyPress)
+    window.removeEventListener("keydown", onKeyPress);
+  }
 }
-} 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 const imagesMarkup = createImagesMarkup(galleryItems);
 
-
-
-galleryEl.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryEl.insertAdjacentHTML("beforeend", imagesMarkup);
 
 function createImagesMarkup(galleryItems) {
-    return galleryItems.map(image => `<a class="gallery__item" href="${image.original}">
+  return galleryItems
+    .map(
+      (image) => `<a class="gallery__item" href="${image.original}">
      <img class="gallery__image" src="${image.preview}" data-source="${image.original}" 
-     alt="${image.description}"/></a>`).join('');
+     alt="${image.description}"/></a>`
+    )
+    .join("");
 }
 
-let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
 
-lightbox.on('show.simplelightbox', () => {});
-
+lightbox.on("show.simplelightbox", () => {});
